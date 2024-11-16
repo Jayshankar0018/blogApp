35974db8741200390df61bd77a0fcb773ddabf92
jayshankar <139064975+Jayshankar0018@users.noreply.github.com>
2024-11-17 00:50:13 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <div className="logo mx-4 w-[38px] overflow-hidden hover:w-[30%] transition-all duration-300 ease-in-out ml-[20vw]">
                     <h1 className='text-[60px] tracking-wider font-bold cursor-pointer' onClick={()=> {
                         navigate('/')
-                    }}>JAY</h1>
+                    }}>J A Y</h1>
                 </div>
             </div>
             <Outlet />
